fcf807ab869710a9e205ca8ea1a0705486d6ab85
kjuiop <copying_y@naver.com>
2023-03-16 23:56:21 +0900

    [#3] refactor: springboot Application 어노테이션 삭제 및 containerless application 시작

--- src/main/java/io/gig/springboot/TobySpringBootApplication.java
+++ src/main/java/io/gig/springboot/TobySpringBootApplication.java
@@ -1,16 +1,12 @@
 package io.gig.springboot;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 /**
  * @author : JAKE
  * @date : 2023/03/12
  */
-@SpringBootApplication
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TobySpringBootApplication.class, args);
+        System.out.println("Hello Containerless Standalone Application");
     }
 }
