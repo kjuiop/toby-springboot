6a2ba36f0234e532c076b34778d8d7e3d1c3a932
kjuiop <copying_y@naver.com>
2023-03-18 09:52:03 +0900

    [#3] feat: servlet container 로 health-check api 기능 구현

--- src/main/java/io/gig/springboot/TobySpringBootApplication.java
+++ src/main/java/io/gig/springboot/TobySpringBootApplication.java
@@ -1,5 +1,15 @@
 package io.gig.springboot;
 
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * @author : JAKE
  * @date : 2023/03/12
@@ -7,6 +17,19 @@ package io.gig.springboot;
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
-        System.out.println("Hello Containerless Standalone Application");
+        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("health-check", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setHeader("Content-Type", "text/plain");
+                    resp.getWriter().print("status OK");
+                }
+            }).addMapping("/health-check");
+        });
+        webServer.start();
     }
 }
+
+
