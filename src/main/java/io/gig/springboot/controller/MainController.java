a34a85d68a2f9479e4eb6df40123fed74f301ade
kjuiop <copying_y@naver.com>
2023-04-12 23:55:51 +0900

    [#5] feat: 의존 오브젝트 추가

--- src/main/java/io/gig/springboot/controller/MainController.java
+++ src/main/java/io/gig/springboot/controller/MainController.java
@@ -1,5 +1,9 @@
 package io.gig.springboot.controller;
 
+import io.gig.springboot.service.SimpleHelloService;
+
+import java.util.Objects;
+
 /**
  * @author : JAKE
  * @date : 2023/03/16
@@ -7,6 +11,7 @@ package io.gig.springboot.controller;
 public class MainController {
 
     public String healthCheck(String name) {
-        return "Status ok " + name;
+        SimpleHelloService helloService = new SimpleHelloService();
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
---
+++ src/main/java/io/gig/springboot/service/SimpleHelloService.java
@@ -0,0 +1,12 @@
+package io.gig.springboot.service;
+
+/**
+ * @author : JAKE
+ * @date : 2023/04/12
+ */
+public class SimpleHelloService {
+
+    public String sayHello(String name) {
+        return "Hello " + name;
+    }
+}
