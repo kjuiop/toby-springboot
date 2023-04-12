1cccb02e2d01d1792312bc93ebeaaefa3932f8ed
kjuiop <copying_y@naver.com>
2023-04-12 23:32:56 +0900

    [#5] feat: 서블릿컨테이너에서 스프링컨테이너로 변경

--- src/main/java/io/gig/springboot/TobySpringBootApplication.java
+++ src/main/java/io/gig/springboot/TobySpringBootApplication.java
@@ -4,6 +4,7 @@ import io.gig.springboot.controller.MainController;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
@@ -23,10 +24,21 @@ import java.io.IOException;
 public class TobySpringBootApplication {
 
     public static void main(String[] args) {
+
+        // 스프링컨테이너를 대표하는 애플리케이션 컨텍스트,
+        // 리소스 접근, 내부 이벤트 전달, 구독하는 방법 등을 수행함.
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        // object 가 아닌 클래스 정보만 넘김
+        applicationContext.registerBean(MainController.class);
+        // bean object 를 생성함.
+        applicationContext.refresh();
+
+
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-            MainController mainController = new MainController();
+            // Spring Container 에서 직접 mainController 를 다루지 않음
+//            MainController mainController = new MainController();
 
 
             servletContext.addServlet("frontcontroller", new HttpServlet() {
@@ -37,10 +49,17 @@ public class TobySpringBootApplication {
                     if (req.getRequestURI().equals("/health-check") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        MainController mainController = applicationContext.getBean(MainController.class);
                         String result = mainController.healthCheck(name);
+                        // 직접 클래스를 의존하지 않고, 등록된 빈을 가져오는 방식으로 진행됨.
+                        // String result = mainController.healthCheck(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        // 컨테이너에서 성공으로 리턴함.
+                        // resp.setStatus(HttpStatus.OK.value());
+
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        // resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+
                         resp.getWriter().print(result);
                     }
                     else if (req.getRequestURI().equals("/users")) {
