d6a163753aedec7f68505e8340ff67bbe700085e
kjuiop <copying_y@naver.com>
2023-04-15 19:17:27 +0900

    [#5] feat: applicationContext 주입 하여 적용 실습

--- src/main/java/io/gig/springboot/TobySpringBootApplication.java
+++ src/main/java/io/gig/springboot/TobySpringBootApplication.java
@@ -3,6 +3,7 @@ package io.gig.springboot;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
@@ -17,50 +18,94 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class TobySpringBootApplication {
 
-    // 이전에는 @Bean 을 선언하여 해당 메서드를 디스패처서블릿에 등록하였는데,
-    // @Component 라는 어노테이션만 사용하면 @Bean 메서드를 일일이 등록할 필요 없이 전체 클래스가 등록된다.
-    // @ComponentScan 을 애플리케이션 쪽에 붙여놓으면 @Component 를 탐색해서 등록한다.
-    // 애플리케이션에서 사용되는 클래스가 무엇인지 일일이 탐색해야한다.
-    // 메타애노테이션은 애노테이션 위에 붙은 애노테이션이다.
-    public static void main(String[] args) {
+    @Bean
+    public ServletWebServerFactory serverFactory() {
+        return new TomcatServletWebServerFactory();
+    }
 
-        // 스프링컨테이너를 대표하는 애플리케이션 컨텍스트,
-        // refresh 는 컨테이너 초기화 기능
-        // refresh 가 일어나는 중 부가적으로 어떤 작업이 필요하다면 만든 것이 있음
-        // 익명클래스 사용 -> 클래스 뒤에 {} 작성해서 사용
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        return new DispatcherServlet();
+    }
+
+    public static void main(String[] args) {
         AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
 
-            // 기존에는 외부에서 변수를 생성하여 주입하였으나, 이제는 자기 자신 안에서 사용
             @Override
             protected void onRefresh() {
                 super.onRefresh();
 
-                ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+                ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+                DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
+                // SpringContainer 가 dispatcherServlet 이 applicationContext 가 필요하다는 걸 알고 주입해준다.
+                // dispatcherServlet.setApplicationContext(this);
+
                 WebServer webServer = serverFactory.getWebServer(servletContext -> {
-
-                    // springServlet 과 연동하기 위함
-                    // web 요청에 대한 정보를 오브젝트와 연결시키는 정보를 넘겨주지 않았음.
-                    // 우리가 생성한 helloController 의 Object 를 어떤 url 과 매핑하여 연결시킬 정보를 전달하지 않았음
-                    // 이걸 web.xml 등으로 전달했었음
-                    // 매핑 정보를 web.xml 에 적어서 전달하지 않고, 컨트롤러에 명시하는 방법을 선택했다.
-                    servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
+                    servletContext.addServlet("dispatcherServlet", dispatcherServlet
                     ).addMapping("/*");
-
                 });
+
                 webServer.start();
             }
         };
 
-
-        // applicationContext.registerBean(MainController.class);
-        // applicationContext.registerBean(SimpleHelloService.class);
-        // 처음 GenericWebApplicationContext 에서는 수동으로 Bean 을 모두 등록했었지만 AnnotationConfigWebApplicationContext 를
-        // 사용하면, @Configuration 어노테이션이 붙은 클래스에서 @Bean 으로 등록한 팩토리 메서드들을 자동으로 등록해준다.
         applicationContext.register(TobySpringBootApplication.class);
-
         applicationContext.refresh();
     }
+
+
 }
+
+
+/**
+ *
+ *
+ // 이전에는 @Bean 을 선언하여 해당 메서드를 디스패처서블릿에 등록하였는데,
+ // @Component 라는 어노테이션만 사용하면 @Bean 메서드를 일일이 등록할 필요 없이 전체 클래스가 등록된다.
+ // @ComponentScan 을 애플리케이션 쪽에 붙여놓으면 @Component 를 탐색해서 등록한다.
+ // 애플리케이션에서 사용되는 클래스가 무엇인지 일일이 탐색해야한다.
+ // 메타애노테이션은 애노테이션 위에 붙은 애노테이션이다.
+ public static void main(String[] args) {
+
+ // 스프링컨테이너를 대표하는 애플리케이션 컨텍스트,
+ // refresh 는 컨테이너 초기화 기능
+ // refresh 가 일어나는 중 부가적으로 어떤 작업이 필요하다면 만든 것이 있음
+ // 익명클래스 사용 -> 클래스 뒤에 {} 작성해서 사용
+ AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
+
+ // 기존에는 외부에서 변수를 생성하여 주입하였으나, 이제는 자기 자신 안에서 사용
+ @Override
+ protected void onRefresh() {
+ super.onRefresh();
+
+ ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+ WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+ // springServlet 과 연동하기 위함
+ // web 요청에 대한 정보를 오브젝트와 연결시키는 정보를 넘겨주지 않았음.
+ // 우리가 생성한 helloController 의 Object 를 어떤 url 과 매핑하여 연결시킬 정보를 전달하지 않았음
+ // 이걸 web.xml 등으로 전달했었음
+ // 매핑 정보를 web.xml 에 적어서 전달하지 않고, 컨트롤러에 명시하는 방법을 선택했다.
+ servletContext.addServlet("dispatcherServlet", new DispatcherServlet(this)
+ ).addMapping("/*");
+
+ });
+ webServer.start();
+ }
+ };
+
+
+ // applicationContext.registerBean(MainController.class);
+ // applicationContext.registerBean(SimpleHelloService.class);
+ // 처음 GenericWebApplicationContext 에서는 수동으로 Bean 을 모두 등록했었지만 AnnotationConfigWebApplicationContext 를
+ // 사용하면, @Configuration 어노테이션이 붙은 클래스에서 @Bean 으로 등록한 팩토리 메서드들을 자동으로 등록해준다.
+ applicationContext.register(TobySpringBootApplication.class);
+
+ applicationContext.refresh();
+ }
+ *
+ *
+ */
 
 /**
  *     // Object 로 구성할 객체를 생성할 때, HelloService 인터페이스를 찾아서 파라미터로 넣어줘
--- src/main/java/io/gig/springboot/controller/MainController.java
+++ src/main/java/io/gig/springboot/controller/MainController.java
@@ -3,7 +3,8 @@ package io.gig.springboot.controller;
 import io.gig.springboot.annotation.MyComponent;
 import io.gig.springboot.service.HelloService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,14 +17,25 @@ import java.util.Objects;
  */
 @RestController
 @RequestMapping("hello")
-@RequiredArgsConstructor
 @MyComponent
 public class MainController {
 
     private final HelloService helloService;
 
+    // final 로 선언된 객체는 생성자 모두 생성된 이후에 관리하는데, applicationContext 는 그 이전에 생성되서 주입된 것이기 때문에 final 이 안됨
+    private final ApplicationContext applicationContext;
+
+    public MainController(HelloService helloService, ApplicationContext applicationContext) {
+        this.helloService = helloService;
+        this.applicationContext = applicationContext;
+
+        System.out.println(applicationContext);
+    }
+
+
     @GetMapping
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
+
 }
